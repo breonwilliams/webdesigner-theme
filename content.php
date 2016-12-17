5e8023931542775127b107692df6722a0dbcd220
breonw <breon_williams@unc.edu>
2016-12-16 19:26:11 -0500

    modify styles

--- content.php
+++ content.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('padbot-40 marginbot-40'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('padbot-40 marginbot-40 post-border'); ?>>
 	<header class="entry-header">
 		<?php if ('post' == get_post_type()) { ?> 
 		<div class="entry-meta padbot-20">
@@ -8,29 +8,11 @@
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	</header><!-- .entry-header -->
 
-	
-	<?php if (is_search()) { // Only display Excerpts for Search ?> 
-	<div class="entry-summary">
+
+	<div class="entry-summary padbot-20">
 		<?php the_excerpt(); ?>
 		<div class="clearfix"></div>
 	</div><!-- .entry-summary -->
-	<?php } else { ?> 
-	<div class="entry-content padbot-20">
-		<?php the_content(bootstrapBasicMoreLinkText()); ?>
-		<div class="clearfix"></div>
-		<?php 
-		/**
-		 * This wp_link_pages option adapt to use bootstrap pagination style.
-		 * The other part of this pager is in inc/template-tags.php function name bootstrapBasicLinkPagesLink() which is called by wp_link_pages_link filter.
-		 */
-		wp_link_pages(array(
-			'before' => '<div class="page-links">' . __('Pages:', 'bootstrap-basic') . ' <ul class="pagination">',
-			'after'  => '</ul></div>',
-			'separator' => ''
-		));
-		?> 
-	</div><!-- .entry-content -->
-	<?php } //endif; ?> 
 
 	
 	<footer class="entry-meta">
